dbe13e7c73496cdb022c27695937f9ca4c498420
MarwaSamy <marwasamy0998@gmail.com>
2023-09-25 12:53:44 +0300

    Icategory added

---
+++ App/App/ICategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public delegate void add_category(ICategory category);
+    public  interface ICategory
+    {
+     public int Id { get; set; }
+     public string Name { get; set; }
+     public event add_category addeventcategory;
+     public void Add();
+     
+    }
+
